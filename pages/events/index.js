319ddff2296c38d6c60e646942fe8cde6fcb7ccd
tduncan14 <nbatre1@yahoo.com>
2022-06-08 14:33:31 -0700

    keep moving forward

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,18 +1,55 @@
-import React from 'react'
-import Layout from '../../components/Layout'
+import React from 'react';
+import Layout from '../../components/Layout';
+import {API_URL} from '../../config/index';
+import EventItem from '../../components/EventItems';
 
 
-
-function Eventspage(){
+function Eventspage({events}){
 
 
 
     return(
         <Layout title="events">
-            <h1>This an events page</h1>
+           <h1>Events</h1>
+           {events.length === 0 && <h3>Nothing has loaded</h3>}
+
+
+
+           
+                  <h1> Upcoming Events</h1>
+
+
+         {
+            events.length === 0  && <h3> No events to show</h3>
+        }
+
+
+          {events && events.map(event => (
+
+ 
+         <EventItem key={event.id} event={event} />
+     
+
+
+
+             ))}
         </Layout>
     )
 
+}
+
+
+
+export async function getStaticProps(){
+
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json()
+
+
+    return{
+        props:{events},
+        revalidate:1,
+    }
 }
 
 
